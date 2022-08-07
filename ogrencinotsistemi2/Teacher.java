54d4056e053e24d446d271c96c12b6110f104765
Ibrahim M <105627112+359035@users.noreply.github.com>
2022-08-07 12:56:05 +0300

    Add files via upload

---
+++ ogrencinotsistemi2/Teacher.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ogrencinotsistemi2;
+
+/**
+ *
+ * @author Samsung
+ */
+public class Teacher {
+
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+
+}
